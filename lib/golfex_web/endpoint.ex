d880e032ed3073a604f2fbe2c5c33df9f81f6e6f
braidonw <braidon@braidonwhatley.com>
2026-03-28 00:28:31 +1100

    tidewave setup

--- lib/golfex_web/endpoint.ex
+++ lib/golfex_web/endpoint.ex
@@ -27,6 +27,10 @@ defmodule GolfexWeb.Endpoint do
     only: GolfexWeb.static_paths(),
     raise_on_missing_only: code_reloading?
 
+  if Mix.env() == :dev do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
